29821e85afdecb9b134710519d8b87c42ec926cd
Tony Qu <tonyqus@gmail.com>
2014-12-26 15:45:43 +0800

    add ziptextparser and testcase

---
+++ Toxy.Test/ZipParserTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Toxy.Test
+{
+        [TestClass]
+    public class ZipParserTest
+    {
+            [TestMethod]
+            public void TestParseDirectoryFromZip()
+            {
+                ParserContext context = new ParserContext(TestDataSample.GetFilePath("toxy.zip",null));
+                ITextParser parser = ParserFactory.CreateText(context);
+                string list = parser.Parse();
+                Assert.IsNotNull(list);
+                string[] lines = list.Split(new string[]{Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+                Assert.AreEqual(68, lines.Length);
+            }
+    }
+}
---
+++ ToxyFramework/Parsers/ZipTextParser.cs
@@ -0,0 +1,34 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Toxy.Parsers
+{
+    public class ZipTextParser : PlainTextParser
+    {
+        public ZipTextParser(ParserContext context)
+            : base(context)
+        {
+            this.Context = context;
+        }
+        public override string Parse()
+        {
+            if (!File.Exists(Context.Path))
+                throw new FileNotFoundException("File " + Context.Path + " is not found");
+            StringBuilder sb = new StringBuilder();
+            using (ZipInputStream zipStream = new ZipInputStream(File.OpenRead(Context.Path)))
+            {
+                ZipEntry entry = zipStream.GetNextEntry();
+                while (entry != null)
+                {
+                    sb.AppendLine(entry.Name);
+                    entry = zipStream.GetNextEntry();
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
