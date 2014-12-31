c9a26bf98fd91d04d3a03db0cff9ad140db02bcb
Tony Qu <tonyqus@gmail.com>
2014-12-31 16:58:23 +0800

    implement ExcelTextParser
    extract text from Excel 03 and 07

---
+++ ToxyFramework/Parsers/ExcelTextParser.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.Extractor;
+using NPOI.HSSF.Extractor;
+
+namespace Toxy.Parsers
+{
+    public class ExcelTextParser:PlainTextParser
+    {
+        public ExcelTextParser(ParserContext context)
+            : base(context)
+        {
+            
+        }
+        public override string Parse()
+        {
+            if (!File.Exists(Context.Path))
+                throw new FileNotFoundException("File " + Context.Path + " is not found");
+
+            IWorkbook workbook = WorkbookFactory.Create(Context.Path);
+
+            bool extractHeaderFooter = false;
+            if (Context.Properties.ContainsKey("IncludeHeaderFooter"))
+            {
+                extractHeaderFooter = Utility.IsTrue(Context.Properties["IncludeHeaderFooter"]);
+            }
+            bool showCalculatedResult = false;
+            if (Context.Properties.ContainsKey("ShowCalculatedResult"))
+            {
+                showCalculatedResult = Utility.IsTrue(Context.Properties["ShowCalculatedResult"]);
+            }
+            bool includeSheetNames = true;
+            if (Context.Properties.ContainsKey("IncludeSheetNames"))
+            {
+                includeSheetNames = Utility.IsTrue(Context.Properties["IncludeSheetNames"]);
+            }
+            bool includeComment = true;
+            if (Context.Properties.ContainsKey("IncludeComments"))
+            {
+                includeComment = Utility.IsTrue(Context.Properties["IncludeComments"]);
+            }
+
+            if (workbook is XSSFWorkbook)
+            {
+                XSSFExcelExtractor extractor = new XSSFExcelExtractor((XSSFWorkbook)workbook);
+                extractor.SetIncludeHeadersFooters(extractHeaderFooter);
+                extractor.SetIncludeCellComments(includeComment);
+                extractor.SetIncludeSheetNames(includeSheetNames);
+                extractor.SetFormulasNotResults(!showCalculatedResult);
+                return extractor.Text;
+            }
+            else //if (workbook is HSSFWorkbook)
+            {
+                ExcelExtractor extractor = new ExcelExtractor((HSSFWorkbook)workbook);
+                extractor.IncludeHeaderFooter = extractHeaderFooter;
+                extractor.IncludeCellComments= includeComment;
+                extractor.IncludeSheetNames = includeSheetNames;
+                extractor.FormulasNotResults = !showCalculatedResult;
+                return extractor.Text;
+            }
+        }
+    }
+}
