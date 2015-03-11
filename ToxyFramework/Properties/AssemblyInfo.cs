45e30f4e6e9162db7102641dbca4d5131ec07fba
Tony Qu <tonyqus@163.com>
2015-03-12 04:21:31 +0800

    update version to 1.5

--- ToxyFramework/Properties/AssemblyInfo.cs
+++ ToxyFramework/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
-[assembly: AssemblyInformationalVersion("Toxy 1.3")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyInformationalVersion("Toxy 1.5")]
 
 [assembly: AllowPartiallyTrustedCallers]
