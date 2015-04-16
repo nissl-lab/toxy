23ae8badc38ba099881b0f08209107bc33c3215a
suisou <suisou@21cn.com>
2015-04-16 16:54:41 +0800

    加入了spreadsheet的序列化，等待测试

---
+++ ConsoleApplicationTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Toxy.Test;
+namespace ConsoleApplicationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestToxySpreadsheetSerialize   test = new TestToxySpreadsheetSerialize();
+            
+            test.TestSpreadsheetSerialize();
+
+
+        }
+    }
+}
