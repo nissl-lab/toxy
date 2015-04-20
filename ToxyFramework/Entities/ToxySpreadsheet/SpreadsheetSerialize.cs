b2cbbc3f1a510aaf110f86863e72174e7f89644e
suisou <suisou@21cn.com>
2015-04-20 16:26:24 +0800

    增加了存储压缩，测试未通过

--- ToxyFramework/Entities/ToxySpreadsheet/SpreadsheetSerialize.cs
+++ ToxyFramework/Entities/ToxySpreadsheet/SpreadsheetSerialize.cs
@@ -4,16 +4,22 @@ using System.Data;
 using System.Text;
 using Clifton.Tools.Data;
 using System.IO;
-
+using System.IO.Compression;
+using ICSharpCode.SharpZipLib.BZip2;
+using ICSharpCode.SharpZipLib.Checksums;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.GZip;
 
 namespace Toxy
 {
     public partial class ToxySpreadsheet
     {
 
-        public MemoryStream Serialize()
+
+
+        public MemoryStream Serialize(bool bcompress = true)
         {
-            MemoryStream ms=new MemoryStream();
+            MemoryStream ms = new MemoryStream();
             RawSerializer rs = new RawSerializer(ms);
 
             //sheet Serialize
@@ -23,7 +29,7 @@ namespace Toxy
             rs.Serialize(Tables.Count);
             foreach (ToxyTable tb in Tables)
             {
-                
+
                 rs.Serialize(tb.HasHeader);
                 rs.Serialize(tb.Name);
                 rs.SerializeNullable(tb.PageHeader);
@@ -48,7 +54,7 @@ namespace Toxy
                     rs.Serialize(tb.ColumnHeaders.LastCellIndex);
                     //cells
                     rs.Serialize(tb.ColumnHeaders.Cells.Count);
-                    foreach(ToxyCell cell in tb.ColumnHeaders.Cells)
+                    foreach (ToxyCell cell in tb.ColumnHeaders.Cells)
                     {
                         rs.Serialize(cell.Value);
                         rs.Serialize(cell.CellIndex);
@@ -64,12 +70,12 @@ namespace Toxy
                     rs.Serialize(row.LastCellIndex);
                     //cells
                     rs.Serialize(row.Cells.Count);
-                    foreach(ToxyCell cell in row.Cells)
+                    foreach (ToxyCell cell in row.Cells)
                     {
                         rs.Serialize(cell.Value);
                         rs.Serialize(cell.CellIndex);
                         rs.SerializeNullable(cell.Comment);
-                    
+
                     }
 
 
@@ -80,86 +86,136 @@ namespace Toxy
 
             rs.Flush();
 
-            return ms;
+            //http://blog.sina.com.cn/s/blog_40678c33010007zk.html
+
+            byte[] bms = ms.ToArray();
+            MemoryStream ms2=new MemoryStream();
+            Stream compressms = new BZip2OutputStream(ms2);
+             if (bcompress)
+             {
+                 
+
+                 compressms.Write(bms, 0,bms.Length);
+                 compressms.Close();
+                 return ms2;
+             }
+             else
+             {
+                 return ms;
+             }
+
+
+
 
         }
 
 
-        public bool Deserialize(MemoryStream ms)
+
+
+        public bool Deserialize(MemoryStream ms, bool bcompress = true)
         {
-            RawDeserializer rd=new RawDeserializer(ms);
+            
+
+           
+            MemoryStream msd = new MemoryStream();
+            Stream compress = new BZip2InputStream(ms);
+            RawDeserializer rd;
+            if (bcompress)
+            {
+
+                byte[] buf = new byte[1024 ];
+                int count = 0;
+                do
+                {
+                    count = compress.Read(buf, 0, buf.Length);
+                    msd.Write(buf, 0, count);
+                   
+                }
+                while (count > 0);      
+
+
+                rd = new RawDeserializer(msd);
+            }
+            else
+            {
+                rd = new RawDeserializer(ms);
+            }
+
+
+
+
 
             ms.Position = 0;
 
-            
+
 
             //sheet Serialize
-             var   sheetName=rd.DeserializeNullable(typeof(string));
+            var sheetName = rd.DeserializeNullable(typeof(string));
             if (sheetName != null)
             {
                 Name = (string)sheetName;
             }
 
             //table Serialize
-            int TablesCount=rd.DeserializeInt();
-            for (int i =0;i<TablesCount;i++)
+            int TablesCount = rd.DeserializeInt();
+            for (int i = 0; i < TablesCount; i++)
             {
-                ToxyTable tb=new ToxyTable();
-               bool tbHasHeader=rd.DeserializeBool();
-                tb.Name= rd   .DeserializeString()   ;
-                var  tbPageHeader  =rd.DeserializeNullable(typeof(string));
-                if (   tbPageHeader  !=  null )
+                ToxyTable tb = new ToxyTable();
+                bool tbHasHeader = rd.DeserializeBool();
+                tb.Name = rd.DeserializeString();
+                var tbPageHeader = rd.DeserializeNullable(typeof(string));
+                if (tbPageHeader != null)
                 {
                     tb.PageHeader = (string)tbPageHeader;
                 }
-                 var tbPageFooter = rd.DeserializeNullable(typeof(string));
+                var tbPageFooter = rd.DeserializeNullable(typeof(string));
                 if (tbPageFooter != null)
                 {
-                     tb.PageFooter =(string)tbPageFooter;
+                    tb.PageFooter = (string)tbPageFooter;
                 }
-                 
-                tb.LastRowIndex= rd  .DeserializeInt()    ;
+
+                tb.LastRowIndex = rd.DeserializeInt();
                 tb.LastColumnIndex = rd.DeserializeInt();
                 //mergecell 
-               int  tbMergeCellsCount = rd.DeserializeInt();
-               for (int j = 0; j < tbMergeCellsCount;j++ )
-               {
-                   MergeCellRange mcr = new MergeCellRange();
-                   mcr.FirstRow = rd.DeserializeInt();
-                   mcr.LastRow = rd.DeserializeInt();
-                   mcr.FirstColumn = rd.DeserializeInt();
-                   mcr.LastColumn = rd.DeserializeInt();
-                   tb.MergeCells.Add(mcr);
-               }
+                int tbMergeCellsCount = rd.DeserializeInt();
+                for (int j = 0; j < tbMergeCellsCount; j++)
+                {
+                    MergeCellRange mcr = new MergeCellRange();
+                    mcr.FirstRow = rd.DeserializeInt();
+                    mcr.LastRow = rd.DeserializeInt();
+                    mcr.FirstColumn = rd.DeserializeInt();
+                    mcr.LastColumn = rd.DeserializeInt();
+                    tb.MergeCells.Add(mcr);
+                }
 
                 //columnheaders
 
-               if (tbHasHeader)
+                if (tbHasHeader)
                 {
                     tb.ColumnHeaders.RowIndex = rd.DeserializeInt();
                     tb.ColumnHeaders.LastCellIndex = rd.DeserializeInt();
                     //cells
                     int tbColumnHeadersCellsCount = rd.DeserializeInt();
-                    for (int k = 0; k < tbColumnHeadersCellsCount;k++ )
+                    for (int k = 0; k < tbColumnHeadersCellsCount; k++)
                     {
-                        
+
                         string cellValue = rd.DeserializeString();
                         int cellCellIndex = rd.DeserializeInt();
-                        ToxyCell cell = new ToxyCell(cellCellIndex,cellValue);
+                        ToxyCell cell = new ToxyCell(cellCellIndex, cellValue);
                         var cellComment = rd.DeserializeNullable(typeof(string));
-                        if (cellComment   != null)
+                        if (cellComment != null)
                         {
                             cell.Comment = (string)cellComment;
                         }
 
-                        
+
                         tb.ColumnHeaders.Cells.Add(cell);
                     }
                 }
 
                 //rows
                 int tbRowsCount = rd.DeserializeInt();
-                for (int m = 0; m < tbRowsCount;m++ )
+                for (int m = 0; m < tbRowsCount; m++)
                 {
                     int rowRowIndex = rd.DeserializeInt();
                     ToxyRow row = new ToxyRow(rowRowIndex);
@@ -167,7 +223,7 @@ namespace Toxy
                     row.LastCellIndex = rd.DeserializeInt();
                     //cells
                     int rowCellsCount = rd.DeserializeInt();
-                    for (int n = 0; n < rowCellsCount;n++ )
+                    for (int n = 0; n < rowCellsCount; n++)
                     {
 
                         string cellValue = rd.DeserializeString();
