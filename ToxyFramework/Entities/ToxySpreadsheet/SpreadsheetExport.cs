6870a3f5775b4f49e28973e2197af91db8545fd5
suisou <suisou@21cn.com>
2015-04-23 16:22:18 +0800

    spreadsheet  增加了export接口

---
+++ ToxyFramework/Entities/ToxySpreadsheet/SpreadsheetExport.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Clifton.Tools.Data;
+using System.IO;
+using System.IO.Compression;
+using ICSharpCode.SharpZipLib.BZip2;
+using ICSharpCode.SharpZipLib.Checksums;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.GZip;
+
+namespace Toxy
+{
+    public partial class ToxySpreadsheet : ICloneable
+    {
+
+        public bool ToCSV(string filename)
+        {
+
+
+
+
+            return false;
+        }
+
+        public bool ToXLS(string filename)
+        {
+
+            return false;
+        }
+        public bool ToXLSX(string filename)
+        {
+
+            return false;
+        }
+        public bool ToHtml(string filename)
+        {
+
+            return false;
+        }
+
+    }
+}
