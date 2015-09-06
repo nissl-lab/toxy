adeecc971448410c89191bb85bd72b9c76934b20
Tony Qu <tonyqus@163.com>
2015-09-06 22:42:18 +0800

    update toxy assembly version

--- ToxyFramework/Properties/AssemblyInfo.cs
+++ ToxyFramework/Properties/AssemblyInfo.cs
@@ -33,8 +33,5 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
-[assembly: AssemblyInformationalVersion("Toxy 1.5")]
-
-[assembly: AllowPartiallyTrustedCallers]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
