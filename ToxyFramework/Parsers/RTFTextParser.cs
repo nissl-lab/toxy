21281d86fd4cdd1fc3848a20e4c1f76898779658
Flamy <dazenta12e@gmail.com>
2026-02-12 18:30:48 +0100

    removed Streamreader, which wasn't used anymore

--- ToxyFramework/Parsers/RTFTextParser.cs
+++ ToxyFramework/Parsers/RTFTextParser.cs
@@ -19,20 +19,13 @@ namespace Toxy.Parsers
         public string Parse()
         {
             if (!File.Exists(Context.Path))
+            {
                 throw new FileNotFoundException("File " + Context.Path + " is not found");
+            }
 
-            StreamReader sr = null;
-            try
-            {
-                ReasonableRTF.RtfToTextConverter converter = new ReasonableRTF.RtfToTextConverter();
-                ReasonableRTF.Models.RtfResult result = converter.Convert(File.ReadAllBytes(Context.Path));
-                return result.Text;
-            }
-            finally
-            {
-                if (sr != null)
-                    sr.Close();
-            }
+            ReasonableRTF.RtfToTextConverter converter = new ReasonableRTF.RtfToTextConverter();
+            ReasonableRTF.Models.RtfResult result = converter.Convert(File.ReadAllBytes(Context.Path));
+            return result.Text;
         }
     }
 }
