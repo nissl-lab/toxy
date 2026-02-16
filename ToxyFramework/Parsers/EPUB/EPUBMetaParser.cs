a64f177bf66166ee0829ffa24fa5c2e114902651
Flamy <dazenta12e@gmail.com>
2026-02-16 19:03:31 +0100

    adding new Parser for #57

---
+++ ToxyFramework/Parsers/EPUB/EPUBMetaParser.cs
@@ -0,0 +1,35 @@
+﻿using HtmlAgilityPack;
+using System.IO;
+using System.Text;
+using VersOne.Epub;
+
+namespace Toxy.Parsers.EPUB
+{
+    internal class EPUBMetaParser : IMetadataParser
+    {
+        public EPUBMetaParser(ParserContext context)
+        {
+            Context = context;
+        }
+        public virtual ParserContext Context { get; set; }
+        public ToxyMetadata Parse()
+        {
+            if (!File.Exists(Context.Path))
+            {
+                throw new FileNotFoundException("File " + Context.Path + " is not found");
+            }
+
+            StringBuilder result = new StringBuilder();
+            HtmlDocument html = new HtmlDocument();
+            EpubBook book = EpubReader.ReadBook(Context.Path);
+            ToxyMetadata meta = new ToxyMetadata();
+            meta.Add(nameof(book.Author), book.Author);
+            meta.Add(nameof(book.AuthorList), book.AuthorList);
+            meta.Add(nameof(book.Content), book.Content);
+            meta.Add(nameof(book.CoverImage), book.CoverImage);
+            meta.Add(nameof(book.Description), book.Description);
+            meta.Add(nameof(book.Title), book.Title);
+            return meta;
+        }
+    }
+}
