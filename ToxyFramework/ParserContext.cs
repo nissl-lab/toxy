9f783460075f74aefb6474495709e781bace8464
Tony Qu <tonyqus@gmail.com>
2026-02-27 14:13:16 +0800

    Add Extension and MimeType property to ParserContext

--- ToxyFramework/ParserContext.cs
+++ ToxyFramework/ParserContext.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using DocumentFormat.OpenXml.InkML;
+using DocumentFormat.OpenXml.Math;
+using FileSignatures;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -21,9 +24,40 @@ namespace Toxy
         {
             this._filestream = stream;
         }
+        
         public bool IsStreamContext { get=> this._filestream != null; }
         public Stream Stream { get => this._filestream; }
         public string Path { get; set; }
+
+        public string Extension
+        {
+            get
+            {
+                return Utility.GetFileExtention(this);
+            }
+        }
+        public string MimeType
+        {
+            get
+            {
+                FileFormat? fileformat = null;
+                FileFormatInspector inspector = new FileFormatInspector();
+                if (this.IsStreamContext)
+                {
+                    this.Stream.Position = 0;
+                    fileformat = inspector.DetermineFileFormat(this.Stream);
+                }
+                else
+                {
+                    using var stream = File.OpenRead(this.Path);
+                    fileformat = inspector.DetermineFileFormat(stream);
+                }
+                if (fileformat == null)
+                    throw new InvalidDataException("File format could not be determined for the input stream");
+                return fileformat.MediaType;
+
+            }
+        }
         public Encoding Encoding { get; set; }
         
         public Dictionary<string, string> Properties { get; set; }
